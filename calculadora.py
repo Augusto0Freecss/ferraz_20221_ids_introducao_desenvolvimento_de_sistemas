0c186316ae6e2e135e6dc31201183b608a0955a3
Augusto0Freecss <luc2026@gmail.com>
2021-06-01 21:06:55 -0300

    Faz soma de valores digitados

--- calculadora.py
+++ calculadora.py
@@ -1,35 +1,5 @@
-def potencia (a, b):
-  pass
-def divisao (a, b):
-  pass
-def multiplicacao (a, b):
-  pass
-
-def subtracao (a, b):
-  pass
-  return a - b
-
-def soma (a, b):
-  return a + b
 a = int(input("Digite o primeiro valor"))
-b = int(input ("Digite o segundo valor"))
+b = int(input("Digite o segundo valor"))
+resultado = 2 + 3
 
-operacao = input("+: Soma\n-: Subtração\n*: Multiplicação\n/: Divisão\n**: Exponenciação")
-
-if operacao == '+':
-  resultado = soma(a, b)
-
-elif operacao == '-':
-  resultado = a - b
-  resultado = subtracao(a, b)
-
-elif operacao == '*':
-  resultado = a * b
-
-elif operacao == '/':
-  resultado = a // b
-
-else:
-  resultado = a ** b
-
-print (resultado) 
+print(resultado)
